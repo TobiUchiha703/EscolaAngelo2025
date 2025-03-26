dfed0379db2ccc031f454760b4229f1ae723ccd2
Gabriel Zamengo <00001140402407sp@al.educacao.sp.gov.br>
2025-03-26 08:52:59 -0300

    Add files via upload

---
+++ Aula24.py
@@ -0,0 +1,19 @@
+ EXERCÍCIOS FOR
+ 
+ #  1.Faça um programa que imprima cada elemento da lista [28, 9, 22, -31, -3, -31, 10, 32, 10, 2] usando o for.
+ 
+ #  2. Faça um programa que imprima cada elemento da lista [3, 8, 30, 8, 19, -12, -2, -1, -7, -48] usando o for com range.
+ 
+ #  3. Crie uma lista com 10 números inteiros e faça um programa que imprima cada elemento da lista usando o for
+ 
+ #  4. Crie uma lista com 10 números inteiros e faça um programa que imprima cada elemento da lista usando o for com range
+ 
+ # 5. Faça um programa que imprima todos os itens da lista [28, 9, 22, -31, -3, -31, 10, 32, 10, 2] usando while e compare-o com o exercício 1.
+ 
+ #  6. Faça um programa que imprima todos os números de 5 a 0 usando o for.
+ 
+ #  7. Faça um programa que peça para o usuário digitar um número n e imprima uma lista com todos os números de 0 a n-1
+ 
+ # 8. Faça um programa que imprima o maior número da lista [-8, -6, 3, -1, 7], sem usar o método max(). 
+ 
+ # 9. Agora, usando o método max(), faça um programa que imprima os três maiores números da lista [2, 9, -5, 2, -8, 4, -9, -5, 4, 1].
